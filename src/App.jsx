866273e30340c894e92e64d96d0a64e775f968c8
Chamodi Kavishka <154578820+GACK731@users.noreply.github.com>
2026-03-12 10:21:01 +0530

    feat: Add report page and integrate report fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Settings from './pages/Settings';
 import CheckInOut from './pages/CkeckInOut';
 import RegisterStudent from './pages/RegisterStudent';
 import AddRoom from './pages/AddRoom';
+import Report from './pages/Report';
 
 function App() {
   const [activeMenu, setActiveMenu] = useState('Dashboard');
@@ -27,7 +28,7 @@ function App() {
       case 'Check-in/out':
         return <CheckInOut />;
       case 'Reports':
-        return <div className="coming-soon">Reports Page - Coming Soon</div>;
+        return <Report />;
       case 'Settings':
         return <Settings/>;
       case 'Register Student':
